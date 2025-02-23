5aaa600f327b34938c9c100d99272a74fbef14d1
hustacdjm <hustacdjm@gmail.com>
2025-02-23 09:50:26 -0700

    ready for deploy

--- src/app/modules/component/Component4a7cb7f0c7aa463b905d9fc2444dfb15.component.ts
+++ src/app/modules/component/Component4a7cb7f0c7aa463b905d9fc2444dfb15.component.ts
@@ -6,6 +6,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox'
 
 @Component({
     selector     : 'Component4a7cb7f0c7aa463b905d9fc2444dfb15',
@@ -13,7 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
     templateUrl  : './Component4a7cb7f0c7aa463b905d9fc2444dfb15.component.html',
     encapsulation: ViewEncapsulation.None,
     imports:[
-       CommonModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatIconModule,MatRadioModule
+       CommonModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatIconModule,MatCheckboxModule
     ]
     
 })
@@ -25,5 +26,33 @@ export class Component4a7cb7f0c7aa463b905d9fc2444dfb15
      * Constructor
      */
      constructor(public sanitizer:DomSanitizer){}
+
+     isChecked(key:string){
+
+        let answer = this.data.runtime.data.answer;
+        if(answer){
+            return (answer as string[]).includes(key);
+        }
+        return false;
+     }
+
+     onAnswerChange(event: any) {
+
+        let answer = this.data.runtime.data.answer;
+        if(!answer){
+            if(event.checked){
+                this.data.runtime.data.answer=[event.source.value]
+            }            
+        }   
+        else{
+            if(event.checked){
+                this.data.runtime.data.answer=[...this.data.runtime.data.answer,event.source.value]
+            }
+            else{
+                this.data.runtime.data.answer = (this.data.runtime.data.answer as string[]).filter(t=>t!==event.source.value);
+            }
+        }             
+
+     }
 
 }
